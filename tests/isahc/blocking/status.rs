908cc783cb6f7658a9adbf6b663f4039a435d9d6
beltram.maldant <beltram.maldant@soprabanking.com>
2021-06-01 18:13:13 +0200

    test: better tests with panic assertions

--- tests/isahc/blocking/status.rs
+++ tests/isahc/blocking/status.rs
@@ -10,14 +10,35 @@ fn should_assert_status_eq() {
 }
 
 #[test]
+#[should_panic]
+fn assert_status_eq_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/eq.json");
+    get(&srv.uri()).unwrap().assert_status_eq(100);
+}
+
+#[test]
 fn result_should_assert_status_eq() {
     let srv = Stubr::start_blocking("tests/stubs/status/eq.json");
     get(&srv.uri()).assert_status_eq(200);
 }
 
 #[test]
+#[should_panic]
+fn result_assert_status_eq_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/eq.json");
+    get(&srv.uri()).assert_status_eq(100);
+}
+
+#[test]
 fn should_assert_status_ok() {
     let srv = Stubr::start_blocking("tests/stubs/status/ok.json");
+    get(&srv.uri()).unwrap().assert_status_ok();
+}
+
+#[test]
+#[should_panic]
+fn assert_status_ok_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/created.json");
     get(&srv.uri()).unwrap().assert_status_ok();
 }
 
@@ -28,8 +49,22 @@ fn result_should_assert_status_ok() {
 }
 
 #[test]
+#[should_panic]
+fn result_assert_status_ok_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/created.json");
+    get(&srv.uri()).assert_status_ok();
+}
+
+#[test]
 fn should_assert_created() {
     let srv = Stubr::start_blocking("tests/stubs/status/created.json");
+    get(&srv.uri()).unwrap().assert_status_created();
+}
+
+#[test]
+#[should_panic]
+fn assert_created_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/ok.json");
     get(&srv.uri()).unwrap().assert_status_created();
 }
 
@@ -40,8 +75,22 @@ fn result_should_assert_created() {
 }
 
 #[test]
+#[should_panic]
+fn result_assert_created_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/ok.json");
+    get(&srv.uri()).assert_status_created();
+}
+
+#[test]
 fn should_assert_accepted() {
     let srv = Stubr::start_blocking("tests/stubs/status/accepted.json");
+    get(&srv.uri()).unwrap().assert_status_accepted();
+}
+
+#[test]
+#[should_panic]
+fn assert_accepted_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/ok.json");
     get(&srv.uri()).unwrap().assert_status_accepted();
 }
 
@@ -52,13 +101,34 @@ fn result_should_assert_accepted() {
 }
 
 #[test]
+#[should_panic]
+fn result_assert_accepted_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/ok.json");
+    get(&srv.uri()).assert_status_accepted();
+}
+
+#[test]
 fn should_assert_no_content() {
     let srv = Stubr::start_blocking("tests/stubs/status/no-content.json");
     get(&srv.uri()).unwrap().assert_status_no_content();
 }
 
 #[test]
+#[should_panic]
+fn assert_no_content_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/ok.json");
+    get(&srv.uri()).unwrap().assert_status_no_content();
+}
+
+#[test]
 fn result_should_assert_no_content() {
     let srv = Stubr::start_blocking("tests/stubs/status/no-content.json");
+    get(&srv.uri()).assert_status_no_content();
+}
+
+#[test]
+#[should_panic]
+fn result_assert_no_content_should_panic() {
+    let srv = Stubr::start_blocking("tests/stubs/status/ok.json");
     get(&srv.uri()).assert_status_no_content();
 }
--- tests/surf/status.rs
+++ tests/surf/status.rs
@@ -9,15 +9,36 @@ async fn should_assert_status_eq() {
     get(&srv.uri()).await.unwrap().assert_status_eq(200);
 }
 
+#[should_panic]
+#[async_std::test]
+async fn assert_status_eq_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/eq.json").await;
+    get(&srv.uri()).await.unwrap().assert_status_eq(100);
+}
+
 #[async_std::test]
 async fn result_should_assert_status_eq() {
     let srv = Stubr::start("tests/stubs/status/eq.json").await;
     get(&srv.uri()).await.assert_status_eq(200);
 }
 
+#[should_panic]
+#[async_std::test]
+async fn result_assert_status_eq_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/eq.json").await;
+    get(&srv.uri()).await.assert_status_eq(100);
+}
+
 #[async_std::test]
 async fn should_assert_status_ok() {
     let srv = Stubr::start("tests/stubs/status/ok.json").await;
+    get(&srv.uri()).await.unwrap().assert_status_ok();
+}
+
+#[should_panic]
+#[async_std::test]
+async fn assert_status_ok_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/created.json").await;
     get(&srv.uri()).await.unwrap().assert_status_ok();
 }
 
@@ -27,9 +48,23 @@ async fn result_should_assert_status_ok() {
     get(&srv.uri()).await.assert_status_ok();
 }
 
+#[should_panic]
+#[async_std::test]
+async fn result_assert_status_ok_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/created.json").await;
+    get(&srv.uri()).await.assert_status_ok();
+}
+
 #[async_std::test]
 async fn should_assert_status_created() {
     let srv = Stubr::start("tests/stubs/status/created.json").await;
+    get(&srv.uri()).await.unwrap().assert_status_created();
+}
+
+#[should_panic]
+#[async_std::test]
+async fn assert_status_created_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/ok.json").await;
     get(&srv.uri()).await.unwrap().assert_status_created();
 }
 
@@ -39,9 +74,23 @@ async fn result_should_assert_status_created() {
     get(&srv.uri()).await.assert_status_created();
 }
 
+#[should_panic]
+#[async_std::test]
+async fn result_assert_status_created_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/ok.json").await;
+    get(&srv.uri()).await.assert_status_created();
+}
+
 #[async_std::test]
 async fn should_assert_status_accepted() {
     let srv = Stubr::start("tests/stubs/status/accepted.json").await;
+    get(&srv.uri()).await.unwrap().assert_status_accepted();
+}
+
+#[should_panic]
+#[async_std::test]
+async fn assert_status_accepted_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/ok.json").await;
     get(&srv.uri()).await.unwrap().assert_status_accepted();
 }
 
@@ -51,14 +100,35 @@ async fn result_should_assert_status_accepted() {
     get(&srv.uri()).await.assert_status_accepted();
 }
 
+#[should_panic]
+#[async_std::test]
+async fn result_assert_status_accepted_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/ok.json").await;
+    get(&srv.uri()).await.assert_status_accepted();
+}
+
 #[async_std::test]
 async fn should_assert_status_no_content() {
     let srv = Stubr::start("tests/stubs/status/no-content.json").await;
     get(&srv.uri()).await.unwrap().assert_status_no_content();
 }
 
+#[should_panic]
+#[async_std::test]
+async fn assert_status_no_content_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/ok.json").await;
+    get(&srv.uri()).await.unwrap().assert_status_no_content();
+}
+
 #[async_std::test]
 async fn result_should_assert_status_no_content() {
     let srv = Stubr::start("tests/stubs/status/no-content.json").await;
+    get(&srv.uri()).await.assert_status_no_content();
+}
+
+#[should_panic]
+#[async_std::test]
+async fn result_assert_status_no_content_should_panic() {
+    let srv = Stubr::start("tests/stubs/status/ok.json").await;
     get(&srv.uri()).await.assert_status_no_content();
 }
