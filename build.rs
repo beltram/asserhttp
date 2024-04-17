8c5635ae834e7de9e2a94f7fa450185f5454dff2
beltram <beltram.maldant@gmail.com>
2024-04-17 08:32:13 +0200

    fix: make build script conditional on grpc & tonic features

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(any(feature = "grpc", feature = "tonic"))]
     for proto in std::fs::read_dir("tests/protos").unwrap() {
         proto.map(|p| p.path()).ok().map(tonic_build::compile_protos).transpose()?;
     }
